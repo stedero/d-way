8e9ea2fbd1882e55dc97767d607d0b76d19278a2
Steef de Rooi <steef@Bakvis.local>
2018-08-04 15:48:51 +0200

    fixed counter value

--- prc/engine.go
+++ prc/engine.go
@@ -9,7 +9,7 @@ import (
 func Exec(job *Job, w io.Writer) {
 	fmt.Fprintf(w, "Starting job for: %s\n", job.document)
 	for i, process := range job.rule.Processes {
-		fmt.Fprintf(w, "executing step %d %s\n", i, process)
+		fmt.Fprintf(w, "executing step %d %s\n", i+1, process)
 	}
 	fmt.Fprintf(w, "Finished job\n")
 }
