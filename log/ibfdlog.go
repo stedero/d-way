62c2f0138a79029ac29ee34cde925d1a993739c0
Steef de Rooi <steef@Bakvis.fritz.box>
2018-08-13 23:04:24 +0200

    comments

--- log/ibfdlog.go
+++ log/ibfdlog.go
@@ -617,9 +617,9 @@ func Panicln(v ...interface{}) {
 }
 
 // Output writes the output for a logging event. The string s contains
-// the text to print after the prefix specified by the flags of the
-// Logger. A newline is appended if the last character of s is not
-// already a newline. Calldepth is the count of the number of
+// the text to print after the level tag and the prefix specified by
+// the flags of the Logger. A newline is appended if the last character of
+// s is not already a newline. Calldepth is the count of the number of
 // frames to skip when computing the file name and line number
 // if Llongfile or Lshortfile is set; a value of 1 will print the details
 // for the caller of Output.
