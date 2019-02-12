069f93be75b62d910de3bb4d35722696a1009924
Steef de Rooi <s.derooi@ibfd.org>
2019-02-12 15:39:04 +0100

    9843: PDF not getting Social DRM statement at bottom of page Added PDF configurations and tests

--- rules_test.go
+++ rules_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"testing"
 
+	"ibfd.org/d-way/rule"
+
 	"ibfd.org/d-way/cfg"
 )
 
@@ -18,23 +20,52 @@ var tests []Test
 
 func init() {
 	tests = []Test{
-		Test{"/banana", "OTHER"},
-		Test{"/data/tns/docs/printversion/pdf/tns_2019-01-25_ar_1.pdf", "PDF"}}
+		Test{"/document/", "OTHER"},
+		Test{"/document/vatst_de", "LINKRESOLVER"},
+		Test{"/linkresolver/static", "OTHER"},
+		Test{"/linkresolver/static/", "OTHER"},
+		Test{"/linkresolver/static/tt_al-de_50_ger_2015_tt__td1", "LINKRESOLVER"},
+		Test{"/linkresolver/static/some/extra/path/tt_al-de_50_ger_2015_tt__td1", "LINKRESOLVER"},
+
+		Test{"/data/tns/docs/html/tns_1988-01-15_au_1.html", "HTML"},
+		Test{"/archive/ggd/html/ggd_2016_ar_1.html", "HTML"},
+		Test{"/data/tns/html/.html", "HTML"},
+		Test{"/data//html/.html", "HTML"},
+		Test{"/archive/ggd/html/ggd_2016_ar_1.htm", "OTHER"},
+		Test{"/archive/knorhaan/banana/html/kh_299.html", "HTML"},
+
+		Test{"/collections/eulaw/pdf/doc1.pdf", "PDF1"},
+		Test{"/collections/ftn/pdf/doc1.pdf", "PDF1"},
+		Test{"/collections/oecd/pdf/doc1.pdf", "PDF1"},
+		Test{"/collections/tni/pdf/doc1.pdf", "PDF1"},
+		Test{"/collections/vatst/pdf/doc1.pdf", "PDF2"},
+		Test{"/collections/eufake/pdf/doc1.pdf", "PDF2"},
+
+		Test{"/data/tns/docs/pdf/tns_1988-01-15_au_1.pdf", "PDF3"},
+		Test{"/data/treaty/docs/pdf/tt_al-de_50_ger_2015_tt__td1.pdf", "PDF3"},
+		Test{"/data/kf/docs/pdf/kf_nl.pdf", "OTHER"},
+		Test{"/collections/vatst/docs/printversion/pdf/vatst_nl.pdf", "PDF4"},
+		Test{"/data/tns/docs/printversion/pdf/tns_1988-01-15_au_1.pdf", "PDF5"},
+		Test{"/data/tns/docs/printversion/pdf/some/extra/path/here/tns_1997-02-20_nl_1.pdf", "PDF5"},
+		Test{"/banana/tns/docs/printversion/pdf/tns_2019-01-25_ar_1.pdf", "OTHER"},
+		Test{"/data/tns/docs/printversion/pdf/tns_2019-01-25_ar_1.pdf", "PDF5"},
+
+		Test{"/collections/kf/excel/kf_dz.xls", "EXCEL"},
+		Test{"/collections//excel/kf_dz.xls", "EXCEL"}}
 }
 
 func TestRules(t *testing.T) {
 	matcher := cfg.GetMatcher()
 	for _, test := range tests {
 		rule := matcher.Match(test.path, "")
-		assert(t, test.ruleName, rule.Name, test.path)
+		assert(t, rule, test.ruleName, test.path)
 	}
 }
 
-func assert(t *testing.T, expected string, actual string, what string) {
-	t.Logf("\tWhen checking path %s", what)
-	if actual == expected {
-		t.Logf("\t\tThe rule should be \"%s\" %s", actual, checkMark)
+func assert(t *testing.T, rule *rule.Rule, expectedName, what string) {
+	if rule.Name == expectedName {
+		t.Logf("\tFor path %s the rule should be \"%s\" %s", what, rule.Name, checkMark)
 	} else {
-		t.Fatalf("\t\tThe rule should be \"%s\" but it was \"%s\" %s", expected, actual, ballotX)
+		t.Fatalf("\tFor path %s the rule should be \"%s\" but it was \"%s\" %s", what, expectedName, rule.Name, ballotX)
 	}
 }
